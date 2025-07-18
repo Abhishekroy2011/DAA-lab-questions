8361a35f4b6e72aec41c0955967746972629ec40
Abhishekroy2011 <abhishekroychowdhury91@gmail.com>
2025-07-18 14:00:31 +0530

    uodate floyd code

---
+++ floyd.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+#define V 4  // Number of vertices
+#define INF 99999  // Representing infinity
+
+void printSolution(int dist[][V]);
+
+void floydWarshall(int graph[][V]) {
+    int dist[V][V], i, j, k;
+
+    // Step 1: Initialize the solution matrix same as the input graph matrix
+    for (i = 0; i < V; i++)
+        for (j = 0; j < V; j++)
+            dist[i][j] = graph[i][j];
+
+    // Step 2: Update distances considering each vertex as an intermediate
+    for (k = 0; k < V; k++) {
+        for (i = 0; i < V; i++) {
+            for (j = 0; j < V; j++) {
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+            }
+        }
+    }
+
+    // Step 3: Print the shortest distance matrix
+    printSolution(dist);
+}
+
+void printSolution(int dist[][V]) {
+    printf("Shortest distances between every pair of vertices:\n");
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                printf("%7s", "INF");
+            else
+                printf("%7d", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    // Sample graph: INF represents no edge between nodes
+    int graph[V][V] = {
+        {0,   5,  INF, 10},
+        {INF, 0,   3,  INF},
+        {INF, INF, 0,   1},
+        {INF, INF, INF, 0}
+    };
+
+    floydWarshall(graph);
+    return 0;
+}
+
